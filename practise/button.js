91a96a65b473d1508c4c7d45a6ce8b8fb9700452
Aman8111 <rajputaman8111@gmail.com>
2024-07-21 19:35:04 +0000

    button

---
+++ practise/button.js
@@ -0,0 +1,7 @@
+
+<button type="button" onClick="myF()" id="a" >Green</button>
+function myF(){
+   let a= document.getElementById("a");
+    a.body.Style.backgroundColor="red";
+}
+console.log(myF());
